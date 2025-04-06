c5bb0416697ad824ffc0b29aceceb564bebfb3ef
Adam <adamgensh@gmail.com>
2025-04-06 12:27:22 +0300

    Add test cases

--- src/project/remvoe_prefix_test.go
+++ src/project/remvoe_prefix_test.go
@@ -47,6 +47,78 @@ func TestRemovePrefix(t *testing.T) {
 			prefix:  "project1/command_1",
 			want:    "command_1",
 		},
+		{
+			name:    "Deep path - prefix before first slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project",
+			want:    "project1/subdir/nested/deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix at first slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/",
+			want:    "subdir/nested/deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix after first slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/sub",
+			want:    "subdir/nested/deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix before second slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subd",
+			want:    "subdir/nested/deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix at second slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subdir/",
+			want:    "nested/deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix after second slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subdir/nest",
+			want:    "nested/deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix before third slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subdir/neste",
+			want:    "nested/deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix at third slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subdir/nested/",
+			want:    "deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix after third slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subdir/nested/de",
+			want:    "deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix before fourth slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subdir/nested/dee",
+			want:    "deep/command_1",
+		},
+		{
+			name:    "Deep path - prefix at fourth slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subdir/nested/deep/",
+			want:    "command_1",
+		},
+		{
+			name:    "Deep path - prefix after fourth slash",
+			command: "project1/subdir/nested/deep/command_1",
+			prefix:  "project1/subdir/nested/deep/com",
+			want:    "command_1",
+		},
 	}
 
 	for _, tt := range tests {
